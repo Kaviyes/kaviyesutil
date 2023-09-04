fd68801928bc700f5d2ca407a72983e9d8f3f6cd
Kaviyes <98654168+Kaviyes@users.noreply.github.com>
2023-09-05 01:48:31 +0800

    Slight Adjustment

--- setup.py
+++ setup.py
@@ -1,16 +1,13 @@
 from setuptools import setup, find_packages
 
-
-# Package metadata
 NAME = 'kaviyesutil'
 VERSION = '0.1.0'
 DESCRIPTION = "A set of functions that's often used with Kaviyes related projects."
 AUTHOR = 'Kaviyes (Karl Vince Reyes)'
 AUTHOR_EMAIL = 'kaviyeslabs@proton.me'
-URL = 'https://github.com/yourusername/your-package-name'  # Replace with your package's GitHub repository URL
-LICENSE = 'MIT'  # Use your chosen license (e.g., MIT, BSD, Apache, etc.)
+URL = 'https://github.com/Kaviyes/kaviyesutil'
+LICENSE = 'MIT'
 
-# Read long description from README.md
 with open('README.md', 'r', encoding='utf-8') as f:
     LONG_DESCRIPTION = f.read()
 
@@ -30,17 +27,15 @@ setup(
     packages=find_packages(),
     keywords=['utility'],
     classifiers=[
-        'Development Status :: 3 - Alpha',  # Adjust as needed
+        'Development Status :: 3 - Alpha',  
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',  # Adjust if using a different license
+        'License :: OSI Approved :: MIT License',  
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.x',  # Add specific versions if necessary
+        'Programming Language :: Python :: 3.x',  
         'Operating System :: Microsoft :: Windows',
         'Operating System :: UNIX'
         'Operating System :: MacOS :: MacOS X'
     ],
-    install_requires=[  # List your package dependencies here
-        # Example: 'numpy>=1.0',
-    ],
-    python_requires='>=3.6',  # Specify the minimum Python version required
+    install_requires=[ ],
+    python_requires='>=3.6',
 )
